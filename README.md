f11994604409abf5ae439ec4d071894cdfd717be
gabrtv <gabriel@opdemand.com>
2011-11-09 15:08:11 -0700

    first pass at rails hello world

---
+++ README.md
@@ -0,0 +1,5 @@
+Rails Hello World
+-----------------
+This example application is designed to be forked and integrated with a Rails template on OpDemand C2.
+
+http://www.opdemand.com/
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+C2RailsHelloworld::Application.config.secret_token = '622cba99a72156c38ada32a84a5ade77b3ed7233a17a0d6289e74de4ac27490344faee9964e6e4f9ea5c6083e84ab45703c3446c3e381f83f10312617d7e75dc'
