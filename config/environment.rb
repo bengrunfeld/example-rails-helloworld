0e10cfc73ad5564a966315a9c537e4e889c735b9
Gabriel Monroy <gabriel@opdemand.com>
2011-11-10 16:51:54 -0700

    hardcoding encoding to utf-8

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 C2RailsHelloworld::Application.initialize!
+
+# set default encoding
+Encoding.default_internal = 'UTF-8'
