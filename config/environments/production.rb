282dd0e7452026ea89ba66a50c34136dbb8550c6
gabrtv <gabriel@opdemand.com>
2012-04-08 16:06:29 -0600

    enable asset compilation at runtime

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ C2RailsHelloworld::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true # changed for OpDemand
 
   # Generate digests for assets URLs
   config.assets.digest = true
