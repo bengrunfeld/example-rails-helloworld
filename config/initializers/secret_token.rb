7c6f0a4cd065aa5dc95272e9a78198c5371009b1
gabrtv <gabriel@opdemand.com>
2012-04-08 14:51:09 -0600

    refresh rails project

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-C2RailsHelloworld::Application.config.secret_token = '622cba99a72156c38ada32a84a5ade77b3ed7233a17a0d6289e74de4ac27490344faee9964e6e4f9ea5c6083e84ab45703c3446c3e381f83f10312617d7e75dc'
+C2RailsHelloworld::Application.config.secret_token = '102e5e5511fdecc996f40a52e1795d74b9fdd12766806a7a530615dc68cac20ca25458dbeba37c9a9450252a8ec2227588c1c6f14c4aa591d4bd01e72b59d496'
